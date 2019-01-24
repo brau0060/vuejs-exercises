0aa504086c2d082d747d15398d5d694a8902c60c
Alexander Braun <brau0060@algonquinlive.com>
2019-01-24 12:35:06 -0500

    added syntax exploration content

--- src/components/PlayGround/PlayGround.js
+++ src/components/PlayGround/PlayGround.js
@@ -1,3 +1,43 @@
 export default {
-  name: 'PlayGround'
+  name: 'PlayGround',
+  data () {
+    return {
+      backgroundClassName: 'red-background',
+      counter: 0,
+      message: 'static message',
+      anchorTag: '<a href="http:://google.ca"> {{ message }}</a>',
+      nameInput: 'No Named',
+    };
+  },
+  computed: {
+    reversMessage() {
+      return this.message.split('').reverse().join('');
+    }
+  },
+
+  watch: {
+nameInput(newValue, oldValue) {
+  /* eslint-disable*/
+  console.log ('old value', oldValue);
+  console.log ('new value', newValue);
 }
+  },
+  methods: {
+    handleClick() {
+    /* eslint-disable */
+    // gets rid of console.log error
+      this.counter++;
+      //string interpolation
+      this.message = `you are clicking this ${this.counter} times!`
+      this.backgroundClassName = this.counter % 2 === 0 ? 'green-background' : 'red-background';
+     
+      // if (this.counter % 2 === 0){
+      //     this.backgroundClassName = 'green-background'
+      // } else {
+      //    this.backgroundClassName = 'red-background'
+      // }
+
+      this.message = this.nameInput
+    },
+  }
+};
--- src/components/PlayGround/PlayGround.vue
+++ src/components/PlayGround/PlayGround.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="hello">
-    <button></button>
+    <!-- v-bind -->
+    <!-- v-bind short code -->
+  <div v-bind:class="backgroundClassName"> Some Class with background</div>
+
+  <!-- v-on -->
+  <button v-on:click="handleClick">Click This</button>
+
+  <!-- v-once -->
+  <div v-once> the first message will never change: {{ message }}</div> 
+  <div> the first message will never change: {{ message }}</div> 
+  <!-- v-html -->
+  <div v-html="anchorTag"></div>
+  <!-- v-model -->
+  <lable for="firstName">First Name</lable>
+  <input type="text" name ="firstName" v-model="nameInput">
+  <!-- computed -->
+  <div>{{ message }} => reversed: {{ reversMessage }}</div>
+
+  <!-- watch -->
+  <input type="text" name ="firstName" v-model="nameInput">
   </div>
 </template>
 
 <script src="./PlayGround.js"></script>
-
-
-<style src="./PlayGround.css" scoped>
-</style>
+<style src="./PlayGround.css" scoped></style>
